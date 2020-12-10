ef2c98a8d0b07c3005ec14bb015f78dc229a28fe
Rico Castelo <rcastelo@rowetechinc.com>
2020-12-10 01:15:02 -0800

    Setup all the plots

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# File Check
-Check the Binary or DB file for any errors in the data.
-This will check things like voltage, heading, pitch and roll spikes, date jumps and other test.
-Streamlit is used to display plots of the results.
+# Read Plot CSV
+Read in the data.  This will create a SQLite database file with all the data read in.
+Then create plots for this data.  Use the parameters to filter the data for the plots.
+The plots will be output using Streamlit and save to HTML file in the same folder as the
+data files.  The data will then be exported to CSV.  There will be a CSV file each datatype (Beam, Instrument, Earth and Mag/Dir).
 
 ![File Check Loading](http://rowetechinc.co/github_img/file-check-loading.png)
 
@@ -17,7 +18,15 @@ pip install -r rti_python\requirements.txt
 pip install -r rti_python_plot\requirements.txt
 ```
 
-# Run Application
+# Run Application 
+```commandline
+python app.py
+```
+
+This version will only create the HTML files and CSV files.  You will not see the data in the web browser.
+
+
+# Run Application With Streamlit
 ```commandline
 # To just see results
 python app.py
@@ -25,3 +34,5 @@ python app.py
 # To see plots also
 streamlit run app.py
 ```
+This version will create the HTML files and CSV files.  YOu can also view and interact with plots in the Streamlit interface.
+
--- app.py
+++ app.py
@@ -1,4 +1,73 @@
 from rti_python.Utilities.read_plot_csv import ReadPlotCSV
 
-to_csv = ReadPlotCSV()
-to_csv.select_and_process(max_roll=160, max_vel=88, max_bt_vel=1.0, max_mag=3.0)
+rpc = ReadPlotCSV()
+
+max_roll = 160.0
+max_vel = 2.0
+max_bt_vel = 1.0
+max_mag = 3.0
+
+# Select the file and process the data to a DB file
+rpc.select_and_process()
+
+filter_data = "WHERE ensembles.roll > " + str(max_roll) + " OR ensembles.roll < -" + str(max_roll)
+
+# Beam Velocity
+rpc.beam_0_vel(filter_data, max_vel=max_vel, display_text=False, display_stats=False, display_plot=False)
+rpc.beam_0_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.beam_1_vel(filter_data, max_vel=max_vel, display_text=False, display_stats=False, display_plot=False)
+rpc.beam_1_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.beam_2_vel(filter_data, max_vel=max_vel, display_text=False, display_stats=False, display_plot=False)
+rpc.beam_2_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.beam_3_vel(filter_data, max_vel=max_vel, display_text=False, display_stats=False, display_plot=False)
+rpc.beam_3_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.display_beam_vel_plots()
+
+# Instrument Velocity
+rpc.intr_x_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.intr_y_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.intr_z_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.intr_err_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.display_instr_vel_plots()
+
+# Earth Velocity
+rpc.earth_east_vel(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.earth_north_vel(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.earth_vert_vel(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.earth_east_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.earth_north_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.earth_vert_vel_remove_ship(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=False, display_stats=True, display_plot=False)
+rpc.display_earth_vel_plots()
+rpc.display_earth_vel_raw_vsr_plots()
+
+# Magnitude and Direction
+rpc.mag_plot(filter_data, max_mag=max_mag, max_bt_vel=max_bt_vel, display_text=True, display_stats=True, display_plot=False)
+rpc.dir_plot(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=True, display_stats=True, display_plot=False)
+rpc.mag_vsr_plot(filter_data, max_mag=max_mag, max_bt_vel=max_bt_vel, display_text=True, display_stats=True, display_plot=False)
+rpc.dir_vsr_plot(filter_data, max_vel=max_vel, max_bt_vel=max_bt_vel, display_text=True, display_stats=True, display_plot=False)
+rpc.display_mag_dir_plots()
+
+# Amplitude
+rpc.amp_b0(filter_data, display_text=False, display_stats=True, display_plot=False)
+rpc.amp_b1(filter_data, display_text=False, display_stats=True, display_plot=False)
+rpc.amp_b2(filter_data, display_text=False, display_stats=True, display_plot=False)
+rpc.amp_b3(filter_data, display_text=False, display_stats=True, display_plot=False)
+rpc.display_amp_plots()
+
+# Correlation
+rpc.corr_b0(filter_data, display_text=False, display_stats=True, display_plot=False)
+rpc.corr_b1(filter_data, display_text=False, display_stats=True, display_plot=False)
+rpc.corr_b2(filter_data, display_text=False, display_stats=True, display_plot=False)
+rpc.corr_b3(filter_data, display_text=False, display_stats=True, display_plot=False)
+rpc.display_corr_plots()
+
+# Display all the plots in a single HTML
+rpc.display_all_plots()
+rpc.display_mag_dir_earth_corr_amp()
+
+# Export the data to CSV
+rpc.export_beam_to_csv(max_roll=max_roll)
+rpc.export_instr_to_csv(max_roll=max_roll)
+rpc.export_earth_to_csv(max_roll=max_roll)
+rpc.export_mag_dir_to_csv(max_roll=max_roll)
+
